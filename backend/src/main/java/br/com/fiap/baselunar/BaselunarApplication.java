1e353e87965205dc1e6c67ce2741c7ae3c4f1812
Tibaass <tifferreira2@gmail.com>
2026-05-23 19:41:00 -0300

    projeto inicial do backend com spring boot

---
+++ backend/src/main/java/br/com/fiap/baselunar/BaselunarApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baselunar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaselunarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaselunarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/fiap/baselunar/BaselunarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baselunar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaselunarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
